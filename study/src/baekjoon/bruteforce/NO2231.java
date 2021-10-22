7bd85351213458a64f5ad80f9c1e00568bf158cf
cladren123 <48821942+cladren123@users.noreply.github.com>
2021-10-23 03:07:01 +0900

    백준 2231 분해합

---
+++ study/src/baekjoon/bruteforce/NO2231.java
@@ -0,0 +1,40 @@
+package baekjoon.bruteforce;
+
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+// 분해합 
+
+public class NO2231 {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        for(int i = 0; i < n+1; i++){
+            int number = i;
+            int sum = 0;
+
+            while(number != 0) {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            if(sum + i == n){
+                result = i;
+                break;
+            }
+
+        }
+
+        System.out.println(result);
+
+
+
+
+    }
+}
