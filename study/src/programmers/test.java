179bda9ec1606afd29f8484bbad86366027202e3
cladren123 <48821942+cladren123@users.noreply.github.com>
2021-11-27 16:24:43 +0900

    프로그래머스 Lv2 오픈채팅방

--- study/src/programmers/test.java
+++ study/src/programmers/test.java
@@ -1,4 +1,13 @@
 package programmers;
 
 public class test {
+
+    public static void main(String[] args) {
+
+        String s = "abcde";
+
+        System.out.println(s.substring(0,5)); // 0~4 마지막 숫자를 포함하지 않는다.
+    }
 }
+
+
