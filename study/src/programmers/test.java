5a008ac987ddeacfca2dcd78b3497923b9d37b64
cladren123 <48821942+cladren123@users.noreply.github.com>
2021-12-07 18:35:08 +0900

    프로그래머스 Lv2 2개 이하로 다른 비트

--- study/src/programmers/test.java
+++ study/src/programmers/test.java
@@ -9,22 +9,10 @@ public class test {
 
     public static void main(String[] args) {
 
-        HashMap<String, ArrayList<Integer>> map = new HashMap<>();
-
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(0);
-
-        map.put("one", arr);
-
-        System.out.println(map.get("one"));
-
-        ArrayList<Integer> arr2 = new ArrayList<>(map.get("one"));
-        arr2.add(2);
+        String one = "123";
 
 
-        map.put("one", arr2);
 
-        System.out.println(map.get("one"));
 
 
 
