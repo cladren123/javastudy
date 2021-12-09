39300606ba70c72a952b779008de90358eab0de1
cladren123 <48821942+cladren123@users.noreply.github.com>
2021-12-10 01:21:43 +0900

    프로그래머스 Lv2 n^2 배열 자르기

---
+++ study/src/programmers/level2/threepage/n2배열자르기.java
@@ -0,0 +1,35 @@
+package programmers.level2.threepage;
+
+import java.util.*;
+
+public class n2배열자르기 {
+
+    public static void main(String[] args) {
+        int n1 =3;
+        long left1 = 2;
+        long right1 = 5;
+
+        int n2 =4;
+        long left2 = 7;
+        long right2 = 14;
+
+        System.out.println(solution(n1,left1,right1));
+        System.out.println(solution(n2,left2,right2));
+
+    }
+
+    // solution의 형식을 변경해도 괜찮다.
+    public static List<Long> solution(int n, long left, long right) {
+        int[] answer = {};
+
+        List<Long> list = new ArrayList<>();
+
+        for(long i = left; i <= right; i++)
+            list.add(Math.max(i/n, i%n) + 1);
+
+
+
+        return list;
+    }
+
+}
