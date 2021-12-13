06ecaf2873e11e306d5b3236a2c4ec9f1543cba7
cladren123 <48821942+cladren123@users.noreply.github.com>
2021-12-13 16:15:43 +0900

    프로그래머스 Lv2 JadenCase 문자열 만들기

---
+++ study/src/programmers/level2/fourpage/JadenCase문자열만들기.java
@@ -0,0 +1,43 @@
+package programmers.level2.fourpage;
+
+public class JadenCase문자열만들기 {
+
+    public static void main(String[] args) {
+        String s1 = "3people unFollowed me";
+        String s2 = "for the last week";
+
+        System.out.println(solution(s1));
+        System.out.println(solution(s2));
+    }
+
+    public static String solution(String s) {
+        String answer = "";
+
+        s = s.toLowerCase();
+
+        int n = s.length();
+
+        int flag = 1;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(flag == 1 && s.charAt(i) != ' ')
+            {
+                char[] arr = s.toCharArray();
+                arr[i] = Character.toUpperCase(arr[i]);
+                s = new String(arr);
+                flag = 0;
+            }
+
+            if(s.charAt(i) == ' ')
+            {
+                flag = 1;
+            }
+        }
+
+        answer = s;
+
+        return answer;
+    }
+
+}
