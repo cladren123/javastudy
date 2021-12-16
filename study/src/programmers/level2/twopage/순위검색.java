445f9b1453250980a49d0e67c4b95b8c68d38dc3
cladren123 <48821942+cladren123@users.noreply.github.com>
2021-12-16 15:50:09 +0900

    프로그래머스 Lv2 순위 검색

--- study/src/programmers/level2/twopage/순위검색.java
+++ study/src/programmers/level2/twopage/순위검색.java
@@ -10,66 +10,99 @@ public class 순위검색 {
         String[] info = {"java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"};
         String[] query = {"java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"};
 
-        System.out.println(Arrays.toString(solution(info, query)));
+        System.out.println((solution(info, query)));
     }
 
-    public static int[] solution(String[] info, String[] query) {
-        int[] answer = {};
-
-        int n = info.length;
-        int m = info.length;
-
-        // 언어, 회원번호
-        HashMap<String, List<Integer>> language = new HashMap<>();
-
-        // 직군, 회원번호
-        HashMap<String, List<Integer>> worker = new HashMap<>();
-
-        // 경력 회원번호
-        HashMap<String, List<Integer>> career = new HashMap<>();
-
-        // 소울푸드 회원번호
-        HashMap<String, List<Integer>> food = new HashMap<>();
-
-        // 회원번호 점수
-        HashMap<Integer, Integer> score = new HashMap<>();
+    // 모든 경우의 수를 담을 map
+    static HashMap<String, ArrayList<Integer>> map;
 
 
-        List<Integer> list = new ArrayList<>();
-        list.add(0);
-        language.put("some", list);
-        System.out.println(language);
+    public static ArrayList<Integer> solution(String[] info, String[] query) {
+        ArrayList<Integer> answer = new ArrayList<>();
 
-        language.get("some").add(1);
-        System.out.println(language);
+        map = new HashMap<>();
+        int ninfo = info.length;
 
-        language.put("be", language.get("some"));
-        System.out.println(language);
-
-
-
-
-        for(int i = 0; i < n; i++)
+        // 모든 경우의 수를 구하기
+        for(int i = 0; i < ninfo; i++)
         {
-            String[] caseone = info[i].split(" ");
-            String lan = caseone[0];
-            String work = caseone[1];
-            String car = caseone[2];
-            String soul = caseone[3];
-            Integer sco = Integer.valueOf(caseone[4]);
+            dfs(0, "", info[i]);
+        }
 
+        int nquery = query.length;
 
+        for(int i = 0; i < nquery; i++)
+        {
+            int result = 0;
+            String[] one = query[i].split(" ");
+            String caseone = one[0] + one[2] + one[4] + one[6];
+            int score = Integer.parseInt(one[7]);
+            // 이분 탐색
 
-
+            System.out.println(caseone);
+            answer.add(bsearch(caseone,score));
 
         }
 
-
-
-
-
-
-
         return answer;
     }
+
+
+    // 모든 경우의 수 조합하는 함수
+    public static void dfs(int stage, String store, String oneinfo)
+    {
+        String[] sp = oneinfo.split(" ");
+        int score = Integer.parseInt(sp[4]);
+
+        // 종단조건
+        if(stage == 4)
+        {
+            // 있는 경우
+            if(map.containsKey(store))
+            {
+                map.get(store).add(score);
+            }
+            // 없는 경우
+            else
+            {
+                ArrayList<Integer> ione = new ArrayList<Integer>();
+                ione.add(score);
+                map.put(store, ione);
+            }
+
+            return;
+        }
+
+        dfs(stage+1, store + "-", oneinfo);
+        dfs(stage+1, store + sp[stage], oneinfo);
+
+    }
+
+
+    // 이분탐색
+    public static int bsearch(String caseone, int score)
+    {
+        int result = 0;
+
+        // 찾는 대상이 map에 없다면 0을 리턴
+        if(!map.containsKey(caseone)) return 0;
+
+        ArrayList<Integer> one = map.get(caseone);
+        Collections.sort(one);
+
+        int start = 0;
+        int end = one.size() - 1;
+
+        while(start <= end)
+        {
+            int mid = (start + end) / 2;
+
+            if(score > one.get(mid)) start = mid + 1;
+            else end = mid - 1;
+        }
+
+
+        return one.size() - start;
+    }
+
 }
