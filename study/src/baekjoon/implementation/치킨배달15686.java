9b8db6bbed69a7846b870a4bab9225dde92f0b69
cladren123 <48821942+cladren123@users.noreply.github.com>
2022-04-09 23:42:57 +0900

    프로그래머스, 백준 문제풀이

---
+++ study/src/baekjoon/implementation/치킨배달15686.java
@@ -0,0 +1,30 @@
+package baekjoon.implementation;
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class 치킨배달15686 {
+
+    static int n;
+    static int m;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+
+
+
+
+
+    }
+
+
+
+}
