5b960b278b0552fd8a02d8773160de86965711f4
cladren123 <48821942+cladren123@users.noreply.github.com>
2022-05-15 17:08:54 +0900

    알고리즘 문제 풀이

---
+++ study/src/studyGroup/양식.java
@@ -0,0 +1,12 @@
+package studyGroup;
+
+import java.util.*;
+import java.io.*;
+
+public class 양식 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+}
